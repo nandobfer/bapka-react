96569e4294c55f9600f9a78a850677657cce7b70
nandobfer <nandobfer@yahoo.com.br>
2022-09-18 01:03:17 -0300

    added signup post

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -5,8 +5,13 @@ import { CustomInput } from '../../components/CustomInput';
 import { MainContainer } from '../../components/MainContainer';
 import './style.scss'
 import colors from '../../_colors.scss'
+import { LoadingScreen } from '../../components/LoadingScreen';
+import { useState } from 'react';
+import { api } from '../../api';
 
 export const Cadastro = () => {
+
+    const [loading, setLoading] = useState(false);
     const location = useLocation();
     const parceiro = location.state;
     const navigate = useNavigate();
@@ -38,44 +43,67 @@ export const Cadastro = () => {
     buildInputs();
 
     const sendSignUpForm = (values) => {
-        console.log(values)
+        setLoading(true)
+        const data = {
+            id: parceiro.id,
+            cliente: values
+        }
+        console.log(data)
+
+        api.post('/new_client/', data).then((response) => {
+            console.log(response.data)
+            if (response.data.error) {
+                alert(response.data.error)
+            } else {
+                alert(`Sucesso: ${response.data}`)
+            }
+            setLoading(false)
+        }).catch((error) => {
+            console.log(error)
+            alert(error);
+            setLoading(false)
+        })
+
     }
 
     return (
-        <MainContainer>
-            <section className="signup-page">
-                <div>
-                    <img src="/logo.webp" alt="logo" />
-                    <h1>Cadastro do cliente</h1>
-                    <h2>Preencha os campos ao lado.</h2>
-                    <p>Aqui você irá cadastrar os clientes que estão aptos a receber os cupons da promoção</p>
+        <section>
+            <LoadingScreen loading={loading} />
+            <MainContainer>
+                <section className="signup-page">
                     <div>
-                        <CustomButton
-                            text="Voltar"
-                            action={goBack}
-                            border={`0.2vw solid ${colors.background_color}`}
-                        />
-                    </div>
-                </div>
-                <form onSubmit={formik.handleSubmit}>
-                    {inputs.map(item => {
-                        return (
-                            <CustomInput
-                                id={item}
-                                type='text'
-                                placeholder={`${item.split('_')[1][0].toUpperCase()}${item.split('_')[1].slice(1)}`}
-                                formik={formik}
-                                key={inputs.indexOf(item)}
+                        <img src="/logo.webp" alt="logo" />
+                        <h1>Cadastro do cliente</h1>
+                        <h2>Preencha os campos ao lado.</h2>
+                        <p>Aqui você irá cadastrar os clientes que estão aptos a receber os cupons da promoção</p>
+                        <div>
+                            <CustomButton
+                                text="Voltar"
+                                action={goBack}
+                                border={`0.2vw solid ${colors.background_color}`}
                             />
-                        )
-                    })}
-                    <div>
-                        <CustomButton
-                            text='Cadastrar cliente'
-                        />
+                        </div>
                     </div>
-                </form>
-            </section>
-        </MainContainer>
+                    <form onSubmit={formik.handleSubmit}>
+                        {inputs.map(item => {
+                            return (
+                                <CustomInput
+                                    id={item}
+                                    type='text'
+                                    placeholder={`${item.split('_')[1][0].toUpperCase()}${item.split('_')[1].slice(1)}`}
+                                    formik={formik}
+                                    key={inputs.indexOf(item)}
+                                />
+                            )
+                        })}
+                        <div>
+                            <CustomButton
+                                text='Cadastrar cliente'
+                            />
+                        </div>
+                    </form>
+                </section>
+            </MainContainer>
+        </section>
     )
 }
