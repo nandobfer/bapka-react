a5092ba5c660697577581879cc74a0eba784bcec
nandobfer <nandobfer@yahoo.com.br>
2022-09-17 23:50:42 -0300

    added cadastrar route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ResetPassword from "./pages/ResetPassword";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Cliente } from "./pages/Cliente";
 import { Parceiro } from "./pages/Parceiro";
+import { Cadastro } from "./pages/Cadastro";
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
                 <Route path='/recuperar_senha/' element={<ResetPassword />} />
                 <Route path='/cliente/' element={<Cliente />} />
                 <Route path='/parceiro/' element={<Parceiro />} />
+                <Route path='/cadastrar/' element={<Cadastro />} />
             </Routes>
         </BrowserRouter>
     </section>
