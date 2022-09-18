33e89204e8ee605bfd4c791ef47352ab58d94ed9
nandobfer <nandobfer@yahoo.com.br>
2022-09-18 16:58:30 -0300

    boosted custom button

--- src/components/CustomButton/CustomButton.jsx
+++ src/components/CustomButton/CustomButton.jsx
@@ -1,12 +1,14 @@
 import colors from '../../_colors.scss'
 import './CustomButton.scss'
 
-export const CustomButton = ({text, action, border, font_size, width, height}) => {
+export const CustomButton = ({text, action, border, font_size, width, height, deactivated}) => {
     const style = {
         border: border ? `${border} solid ${colors.background_color}` : null,
         fontSize: font_size ? font_size : null,
         width: width ? width : null,
         height: height ? height : null,
+        opacity: deactivated ? '0.3' : null,
+        pointerEvents: deactivated ? 'none' : null,
     }
     console.log(style)
     return (
