8dfc9c1e2e8169bc4d92d87c5f8bc05b6649922b
nandobfer <nandobfer@yahoo.com.br>
2022-09-18 02:28:36 -0300

    moved background component to pre-route

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Cliente } from "./pages/Cliente";
 import { Parceiro } from "./pages/Parceiro";
 import { Cadastro } from "./pages/Cadastro";
+import { Background } from "./components/Background";
 
 
 const App = () => {
 
   return (
     <section>
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Login />} />
-                <Route path='/recuperar_senha/' element={<ResetPassword />} />
-                <Route path='/cliente/' element={<Cliente />} />
-                <Route path='/parceiro/' element={<Parceiro />} />
-                <Route path='/cadastrar/' element={<Cadastro />} />
-            </Routes>
-        </BrowserRouter>
+        <Background>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Login />} />
+                    <Route path='/recuperar_senha/' element={<ResetPassword />} />
+                    <Route path='/cliente/' element={<Cliente />} />
+                    <Route path='/parceiro/' element={<Parceiro />} />
+                    <Route path='/cadastrar/' element={<Cadastro />} />
+                </Routes>
+            </BrowserRouter>
+        </Background>
     </section>
         
 
--- src/components/MainContainer/index.jsx
+++ src/components/MainContainer/index.jsx
@@ -4,11 +4,9 @@ import './style.scss'
 export const MainContainer = ({children}) => {
     return (
         <section>
-            <Background>
                 <div className="main-container">
                     {children}
                 </div>
-            </Background>
         </section>
     )
 }
