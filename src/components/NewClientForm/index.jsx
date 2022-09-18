64627e62fafbfb987d54b361fc773993a58c76bd
nandobfer <nandobfer@yahoo.com.br>
2022-09-18 03:14:02 -0300

    componentized signupform

---
+++ src/components/NewClientForm/index.jsx
@@ -0,0 +1,75 @@
+import { useFormik } from 'formik';
+import { api } from '../../api';
+import { CustomButton } from '../CustomButton/CustomButton';
+import { CustomInput } from '../CustomInput';
+import './style.scss'
+
+export const NewClientForm = ({parceiro, setLoading}) => {
+    let inputs = [];
+
+    const formik = useFormik({
+        initialValues: {
+            input_nome: '',
+            input_telefone: '',
+            input_email: '',
+            input_cpf: '',
+            input_senha: '',
+            input_confirmacao: '',
+        },
+        onSubmit: values => sendSignUpForm(values),
+    });
+
+    const buildInputs = () => {
+        for (let key in formik.values) {
+            let input = key;
+            inputs.push(input);
+        }
+        console.log(inputs);
+    }
+    buildInputs();
+
+    const sendSignUpForm = (values) => {
+        setLoading(true);
+        const data = {
+            id_parceiro: parceiro.id,
+            cliente: values
+        };
+        console.log(data);
+
+        api.post('/new_client/', data).then((response) => {
+            console.log(response.data);
+            if (response.data.error) {
+                alert(response.data.error);
+            } else {
+                alert(`Sucesso: ${response.data}`);
+            }
+            setLoading(false);
+        }).catch((error) => {
+            console.log(error);
+            alert(error);
+            setLoading(false);
+        })
+    }
+
+    return (
+        <form className='new-client-form' onSubmit={formik.handleSubmit}>
+            {inputs.map(item => {
+                return (
+                    <CustomInput
+                        id={item}
+                        type='text'
+                        placeholder={`${item.split('_')[1][0].toUpperCase()}${item.split('_')[1].slice(1)}`}
+                        formik={formik}
+                        key={inputs.indexOf(item)}
+                        required={true}
+                    />
+                )
+            })}
+            <div>
+                <CustomButton
+                    text='Cadastrar cliente'
+                />
+            </div>
+        </form>
+    )
+}
--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -8,6 +8,7 @@ import colors from '../../_colors.scss'
 import { LoadingScreen } from '../../components/LoadingScreen';
 import { useState } from 'react';
 import { api } from '../../api';
+import { NewClientForm } from '../../components/NewClientForm';
 
 export const Cadastro = () => {
 
@@ -15,56 +16,11 @@ export const Cadastro = () => {
     const location = useLocation();
     const parceiro = location.state;
     const navigate = useNavigate();
-    let inputs = [];
 
     const goBack = () => {
         navigate(-1);
     }
 
-    const formik = useFormik({
-        initialValues: {
-            input_nome: '',
-            input_telefone: '',
-            input_email: '',
-            input_cpf: '',
-            input_senha: '',
-            input_confirmacao: '',
-        },
-        onSubmit: values => sendSignUpForm(values),
-    });
-
-    const buildInputs = () => {
-        for (let key in formik.values) {
-            let input = key;
-            inputs.push(input);
-        }
-        console.log(inputs);
-    }
-    buildInputs();
-
-    const sendSignUpForm = (values) => {
-        setLoading(true);
-        const data = {
-            id_parceiro: parceiro.id,
-            cliente: values
-        };
-        console.log(data);
-
-        api.post('/new_client/', data).then((response) => {
-            console.log(response.data);
-            if (response.data.error) {
-                alert(response.data.error);
-            } else {
-                alert(`Sucesso: ${response.data}`);
-            }
-            setLoading(false);
-        }).catch((error) => {
-            console.log(error);
-            alert(error);
-            setLoading(false);
-        })
-
-    }
 
     return (
         <section>
@@ -84,24 +40,10 @@ export const Cadastro = () => {
                             />
                         </div>
                     </div>
-                    <form onSubmit={formik.handleSubmit}>
-                        {inputs.map(item => {
-                            return (
-                                <CustomInput
-                                    id={item}
-                                    type='text'
-                                    placeholder={`${item.split('_')[1][0].toUpperCase()}${item.split('_')[1].slice(1)}`}
-                                    formik={formik}
-                                    key={inputs.indexOf(item)}
-                                />
-                            )
-                        })}
-                        <div>
-                            <CustomButton
-                                text='Cadastrar cliente'
-                            />
-                        </div>
-                    </form>
+                    <NewClientForm 
+                        parceiro={parceiro}
+                        setLoading={setLoading}
+                    />
                 </section>
             </MainContainer>
         </section>
