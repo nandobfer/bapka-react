b18a057772f026e429460c7ecd8ca237b1184eb9
nandobfer <nandobfer@yahoo.com.br>
2022-09-18 21:58:43 -0300

    boosted build script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
 yarn build
 Write-Output 'Uploading files to hostgator'
 scp -r -P 22022 build/* root@agenciazop.com.br:/home/bapkasorvetes/cupons_webserver/
+ssh hostgator "chown -R bapkasorvetes:bapkasorvetes /home/bapkasorvetes/cupons_webserver/*"
